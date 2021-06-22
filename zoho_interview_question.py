e684d0fe7f516c9e35d50fa8d4752d38b211eb18
divyush001 <72906181+divyush001@users.noreply.github.com>
2021-06-22 10:29:40 +0530

    Add files via upload

---
+++ zoho_interview_question.py
@@ -0,0 +1,20 @@
+word = 'Water'
+
+lst = []
+
+for i in word:
+    lst.append(i)
+
+print(lst)
+len_of_word = len(lst)
+print(len_of_word)
+
+i=int(len_of_word/2)
+print(i)
+k=0
+while(k<len_of_word):
+    while(j<k):
+        print(" ")
+
+
+    i+=1
